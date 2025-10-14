2c4fd8a89204735ba453242ad58cef0f50f3b2a3
James Hendry <jameshendry05@gmail.com>
2025-10-14 16:52:20 +0100

    Put grammar tests in a separate file

--- src/ast.rs
+++ src/ast.rs
@@ -1,4 +1,10 @@
+use std::num::ParseIntError;
 use std::str::FromStr;
+
+#[derive(Debug)]
+pub enum ParseError {
+    RegErr(ParseRegError),
+}
 
 #[derive(Debug, PartialEq)]
 pub enum RegType {
@@ -22,6 +28,22 @@ impl FromStr for RegType {
 
 type RegWidth = u32;
 
+#[derive(Debug)]
+pub enum ParseRegError {
+    /// The width, count or register index is not an valid integer
+    InvalidInt(ParseIntError),
+    /// The structure of the string is wrong
+    InvalidFormat,
+    /// The type of register is not a known type
+    InvalidRegisterType,
+}
+
+impl From<ParseIntError> for ParseRegError {
+    fn from(value: ParseIntError) -> Self {
+        Self::InvalidInt(value)
+    }
+}
+
 #[derive(Debug, PartialEq)]
 pub enum RegisterSet {
     Inferred,
@@ -29,10 +51,62 @@ pub enum RegisterSet {
     Vector(RegType, RegWidth, u32),
 }
 
+impl FromStr for RegisterSet {
+    type Err = ParseRegError;
+
+    /// Parse a register set:
+    /// - u32
+    /// - i16
+    /// - f64
+    /// - vf32
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.is_empty() {
+            return Err(ParseRegError::InvalidRegisterType);
+        }
+        if s.starts_with('v') {
+            let reg_type: RegType = s[1..2]
+                .parse()
+                .map_err(|_| ParseRegError::InvalidRegisterType)?;
+            let (width, count) = s[2..].split_once('x').ok_or(ParseRegError::InvalidFormat)?;
+            let width: RegWidth = width.parse()?;
+            let count: u32 = count.parse()?;
+            Ok(RegisterSet::Vector(reg_type, width, count))
+        } else {
+            let reg_type: RegType = s[0..1]
+                .parse()
+                .map_err(|_| ParseRegError::InvalidRegisterType)?;
+            let width: RegWidth = s[1..].parse()?;
+            Ok(RegisterSet::Single(reg_type, width))
+        }
+    }
+}
+
 #[derive(Debug, PartialEq)]
 pub struct RegisterOperand {
     pub set: RegisterSet,
     pub index: u32,
+}
+
+impl FromStr for RegisterOperand {
+    type Err = ParseRegError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (reg_set, index) = s.split_once(':').ok_or(ParseRegError::InvalidFormat)?;
+        let index: u32 = index.parse().map_err(ParseRegError::InvalidInt)?;
+
+        if reg_set.is_empty() {
+            return Ok(Self {
+                set: RegisterSet::Inferred,
+                index,
+            });
+        }
+
+        let reg_set: RegisterSet = reg_set.parse()?;
+        Ok(Self {
+            set: reg_set,
+            index,
+        })
+    }
 }
 
 #[derive(Debug, PartialEq)]
@@ -46,4 +120,42 @@ pub enum Operand {
 pub struct Instruction {
     pub opcode: String,
     pub operands: Vec<Operand>,
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::ast::{RegType, RegisterOperand, RegisterSet};
+
+    #[test]
+    fn parse_reg_operand_inferred() {
+        assert_eq!(
+            RegisterOperand {
+                set: RegisterSet::Inferred,
+                index: 2
+            },
+            ":2".parse().unwrap()
+        )
+    }
+
+    #[test]
+    fn parse_reg_operand_single() {
+        assert_eq!(
+            RegisterOperand {
+                set: RegisterSet::Single(RegType::UnsignedInt, 32),
+                index: 0
+            },
+            "u32:0".parse().unwrap()
+        );
+    }
+
+    #[test]
+    fn parse_reg_operand_vector() {
+        assert_eq!(
+            RegisterOperand {
+                set: RegisterSet::Vector(RegType::Float, 64, 4),
+                index: 0
+            },
+            "vf64x4:0".parse().unwrap()
+        )
+    }
 }
---
+++ src/grammar_tests.rs
@@ -0,0 +1,76 @@
+use crate::ast::{Instruction, Operand, RegType, RegisterOperand, RegisterSet};
+use crate::grammar::{InstructionParser, OperandParser, RegOperandParser};
+
+#[test]
+fn parse_hex_constant() {
+    let parser = OperandParser::new();
+    let exp = Operand::Constant(0x1A);
+    assert_eq!(exp, parser.parse("0x1A").unwrap())
+}
+
+#[test]
+fn parse_binary_constant() {
+    let parser = OperandParser::new();
+    let exp = Operand::Constant(0b101);
+    assert_eq!(exp, parser.parse("0b101").unwrap())
+}
+
+#[test]
+fn parse_base_10_constant() {
+    let parser = OperandParser::new();
+    let exp = Operand::Constant(303);
+    assert_eq!(exp, parser.parse("#303").unwrap());
+}
+
+#[test]
+fn parse_label_operand() {
+    let parser = OperandParser::new();
+    let exp = Operand::Label("LABEL".to_string());
+    assert_eq!(exp, parser.parse(".LABEL").unwrap());
+}
+
+#[test]
+fn parse_reg_operand() {
+    let parser = OperandParser::new();
+    let exp = RegisterOperand {
+        set: RegisterSet::Single(RegType::UnsignedInt, 32),
+        index: 0,
+    };
+    assert_eq!(Operand::Reg(exp), parser.parse("u32:0").unwrap())
+}
+
+#[test]
+fn parse_vector_reg_operand() {
+    let parser = OperandParser::new();
+    let exp = RegisterOperand {
+        set: RegisterSet::Vector(RegType::SignedInt, 64, 8),
+        index: 4,
+    };
+    assert_eq!(Operand::Reg(exp), parser.parse("vi64x8:4").unwrap())
+}
+
+#[test]
+fn parse_implicit_reg_operand() {
+    let parser = RegOperandParser::new();
+    let exp = RegisterOperand {
+        set: RegisterSet::Inferred,
+        index: 0,
+    };
+    assert_eq!(exp, parser.parse(":0").unwrap())
+}
+
+#[test]
+fn parse_instruction() {
+    let parser = InstructionParser::new();
+    let exp = Instruction {
+        opcode: "mov".to_string(),
+        operands: vec![
+            Operand::Reg(RegisterOperand {
+                set: RegisterSet::Single(RegType::UnsignedInt, 32),
+                index: 0,
+            }),
+            Operand::Constant(100),
+        ],
+    };
+    assert_eq!(exp, parser.parse("mov u32:0, #100").unwrap());
+}
